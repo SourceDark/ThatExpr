be323ec46b0337cd20a5d3f5331fa47d96b1c9ff
胡天翔 <sourcedark@gmail.com>
2015-12-30 16:27:06 +0800

    Dynamic destination

---
+++ WindowsClient/LaGeBiaoQing/Utility/WindowsUtility.cs
@@ -0,0 +1,81 @@
+﻿using LaGeBiaoQing.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace LaGeBiaoQing.Utility
+{
+    enum WindowType { WindowTypeQQ, WindowTypeWeChat }
+
+    class WindowsUtility
+    {
+        
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        public static void sendTo(Expr expr, Image image, WindowType windowType)
+        {
+            copyToClipBoard(expr, image);
+
+            if (windowType == WindowType.WindowTypeQQ) { 
+                Process qqProcess = Process.GetProcessesByName("QQ").FirstOrDefault();
+                if (qqProcess != null)
+                {
+                    IntPtr qqHandle = qqProcess.MainWindowHandle;
+                    Console.Out.WriteLine(qqProcess.ProcessName);
+                    SetForegroundWindow(qqHandle);
+                    SendKeys.SendWait("^V");
+                }
+                else
+                {
+                    MessageBox.Show("Error Message", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else if (windowType == WindowType.WindowTypeWeChat)
+            {
+                Process weChatProcess = Process.GetProcessesByName("WeChat").FirstOrDefault();
+                if (weChatProcess != null)
+                {
+                    IntPtr weChatHandle = weChatProcess.MainWindowHandle;
+                    Console.Out.WriteLine(weChatProcess.ProcessName);
+                    SetForegroundWindow(weChatHandle);
+                    SendKeys.SendWait("^V");
+                }
+                else
+                {
+                    MessageBox.Show("Error Message", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+            // Update recently used exprs
+            List<Expr> recentlyUsedExprs = SettingUtility.getRecentlyUsedExprs();
+            for (int i = 0; i < recentlyUsedExprs.Count; i++)
+            {
+                if (recentlyUsedExprs[i].id == expr.id)
+                {
+                    recentlyUsedExprs.Remove(recentlyUsedExprs[i]);
+                }
+            }
+            recentlyUsedExprs.Insert(0, expr);
+            SettingUtility.setRecentlyUsedExprs(recentlyUsedExprs);
+            if (SettingUtility.exprsDisplayer != null)
+            {
+                SettingUtility.exprsDisplayer.loadRecentlyUsedExprs();
+            }
+        }
+
+        public static void copyToClipBoard(Expr expr, Image image)
+        {
+            string[] s = new string[1];
+            s[0] = FileUtility.FullPath(expr);
+            DataObject dataObject = new DataObject();
+            dataObject.SetData(DataFormats.FileDrop, s);
+            dataObject.SetImage(image);
+            Clipboard.SetDataObject(dataObject);
+        }
+    }
+}
--- WindowsClient/LaGeBiaoQing/View/ExprDisplayer.cs
+++ WindowsClient/LaGeBiaoQing/View/ExprDisplayer.cs
@@ -19,8 +19,11 @@ namespace LaGeBiaoQing.View.PictureBoxs
             this.expr = expr;
             ContextMenu cm = new ContextMenu();
             cm.MenuItems.Add("发送至QQ窗口");
-            cm.MenuItems.Add("发送至YY窗口");
+            cm.MenuItems.Add("发送至微信窗口");
             cm.MenuItems.Add("-");
+            cm.MenuItems[0].Click += ExprDisplayer_SendToQQ;
+            cm.MenuItems[1].Click += ExprDisplayer_SendToWeChat;
+
 
             MenuItem addTagMenuItem = new MenuItem("收藏至");
             addTagMenuItem.MenuItems.Add("默认");
@@ -30,8 +33,15 @@ namespace LaGeBiaoQing.View.PictureBoxs
             this.Click += ExprDisplayer_Click;
         }
 
-        [DllImport("User32.dll")]
-        static extern int SetForegroundWindow(IntPtr point);
+        private void ExprDisplayer_SendToQQ(object sender, EventArgs e)
+        {
+            WindowsUtility.sendTo(expr, Image, WindowType.WindowTypeQQ);
+        }
+
+        private void ExprDisplayer_SendToWeChat(object sender, EventArgs e)
+        {
+            WindowsUtility.sendTo(expr, Image, WindowType.WindowTypeWeChat);
+        }
 
         private void ExprDisplayer_Click(object sender, EventArgs e)
         {
@@ -39,42 +49,7 @@ namespace LaGeBiaoQing.View.PictureBoxs
             MouseEventArgs me = e as MouseEventArgs;
             if (me.Button == MouseButtons.Left)
             {
-                // Copy to clipboard
-                string[] s = new string[1];
-                s[0] = FileUtility.FullPath(expr);
-                DataObject dataObject = new DataObject();
-                dataObject.SetData(DataFormats.FileDrop, s);
-                dataObject.SetData(DataFormats.Bitmap, pictureBox.Image);
-                Clipboard.SetDataObject(dataObject);
-
-                // Send "ctrl+v" to QQ window
-                Process qqProcess = Process.GetProcessesByName("QQ").FirstOrDefault();
-                if (qqProcess != null)
-                {
-                    IntPtr qqHandle = qqProcess.MainWindowHandle;
-                    SetForegroundWindow(qqHandle);
-                    SendKeys.SendWait("^V");
-                }
-                else
-                {
-                    Console.WriteLine("找不到QQ窗口");
-                }
-
-                // Update recently used exprs
-                List<Expr> recentlyUsedExprs = SettingUtility.getRecentlyUsedExprs();
-                for (int i = 0; i < recentlyUsedExprs.Count; i++)
-                {
-                    if (recentlyUsedExprs[i].id == expr.id)
-                    {
-                        recentlyUsedExprs.Remove(recentlyUsedExprs[i]);
-                    }
-                }
-                recentlyUsedExprs.Insert(0, expr);
-                SettingUtility.setRecentlyUsedExprs(recentlyUsedExprs);
-                if (SettingUtility.exprsDisplayer != null)
-                {
-                    SettingUtility.exprsDisplayer.loadRecentlyUsedExprs();
-                }
+                WindowsUtility.sendTo(expr, pictureBox.Image, WindowType.WindowTypeQQ);
             }
         }
     }
