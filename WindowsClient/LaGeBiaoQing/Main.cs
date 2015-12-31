09d546d07b7cc4a24451a34316ef2f34c0ef8120
sourcedark <sourcedark@gmail.com>
2015-12-31 23:57:28 +0800

    Some icons for tab control

--- WindowsClient/LaGeBiaoQing/Main.cs
+++ WindowsClient/LaGeBiaoQing/Main.cs
@@ -5,13 +5,39 @@ using LaGeBiaoQing.View.Settings;
 using LaGeBiaoQing.Utility;
 using System.Runtime.InteropServices;
 using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace LaGeBiaoQing
 {
     public partial class Main : Form
     {
-               
-        // Functions
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect, // x-coordinate of upper-left corner
+            int nTopRect, // y-coordinate of upper-left corner
+            int nRightRect, // x-coordinate of lower-right corner
+            int nBottomRect, // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+         );
+
+        public void DrawRoundRect(Graphics g, Pen p, float X, float Y, float width, float height, float radius)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddLine(X + radius, Y, X + width - (radius * 2), Y);
+            gp.AddArc(X + width - (radius * 2), Y, radius * 2, radius * 2, 270, 90);
+            gp.AddLine(X + width, Y + radius, X + width, Y + height - (radius * 2));
+            gp.AddArc(X + width - (radius * 2), Y + height - (radius * 2), radius * 2, radius * 2, 0, 90);
+            gp.AddLine(X + width - (radius * 2), Y + height, X + radius, Y + height);
+            gp.AddArc(X, Y + height - (radius * 2), radius * 2, radius * 2, 90, 90);
+            gp.AddLine(X, Y + height - (radius * 2), X, Y + radius);
+            gp.AddArc(X, Y, radius * 2, radius * 2, 180, 90);
+            gp.CloseFigure();
+            g.DrawPath(p, gp);
+            gp.Dispose();
+        }
 
         public Main()
         {
@@ -21,17 +47,22 @@ namespace LaGeBiaoQing
 
         public void InitializeCustomComponent()
         {
+            /*
             tabControl1.Controls.Add(new CollectionTagPage());
             tabControl1.Controls.Add(new DiscoverTabPage());
             tabControl1.Controls.Add(new SettingsTabPage());
-
+            */
+            tabControl1.Hide();
             // Local settings
             this.TopMost = SettingUtility.getIsMainFormTopMost();
 
             // Event
-            this.MouseDown += This_MouseDown;
-        }
+            panel1.MouseDown += This_MouseDown;
 
+            TransparencyKey = Color.Lime;
+            BackColor = Color.Lime;
+        }
+        
         // The following code is to make the form still dragable even if we hide its border
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
@@ -46,6 +77,11 @@ namespace LaGeBiaoQing
                 ReleaseCapture();
                 SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
             }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
